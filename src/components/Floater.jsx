cd423ac7696080fb36d5db6ec0a92b7e2a01edfb
Dinesh Sake <dineshsake12345@gmail.com>
2024-07-08 23:01:36 +0530

    Created menu, working on submenu

--- src/components/Floater.jsx
+++ src/components/Floater.jsx
@@ -2,7 +2,7 @@ import { MessageCircleIcon, PhoneIcon } from "lucide-react"
 
 export const Floater = () => {
   return (
-    <div className="flex flex-col items-center fixed -right-6 top-2/4 -translate-x-2/4 -translate-y-2/4 ">
+    <div className="flex flex-col items-center fixed -right-6 top-2/4 -translate-x-2/4 -translate-y-2/4">
         <div className="p-2 md:p-3 bg-green-700 text-white cursor-pointer transition-all duration-300">
             <MessageCircleIcon
             className="h-5 md:h-6"
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
-import { SmartphoneIcon, MailIcon, MenuIcon } from "lucide-react"
+import { SmartphoneIcon, MailIcon, MenuIcon, PlusIcon } from "lucide-react"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuTrigger, DropdownMenuSeparator, DropdownMenuShortcut} from "./ui/dropdown-menu"
 
 export const Header = () => {
 
@@ -15,17 +16,66 @@ export const Header = () => {
                 <p className="flex justify-center items-center gap-1 font-semibold text-sm"><MailIcon height={16}/>Email : clickhubli@gmail.com</p>
             </div>
         </div>
-        <nav className="bg-[#1e7968] w-full px-4 py-2 flex gap-2 items-center text-white sticky">
-            <MenuIcon height={26} className='cursor-pointer transition duration-200 active:scale-125'
-            />
+        <nav className="bg-[#1e7968] w-full px-4 py-2 flex gap-2 items-center text-white sticky">            
+            <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <MenuIcon height={26} className='cursor-pointer active:scale-125'/>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-[100svw] mt-1 border-b-4 rounded-none border-blue-600">
+                <DropdownMenuGroup className="px-4">
+                    <DropdownMenuItem>
+                        <span className="uppercase font-medium text-[#333] hover:text-black hover:font-semibold">home</span>
+                    </DropdownMenuItem>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem>
+                        <span className="uppercase font-medium text-[#333] hover:text-black hover:font-semibold">about us</span>
+                    </DropdownMenuItem>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem>
+                        <span className="uppercase font-medium text-[#333] hover:text-black hover:font-semibold">products</span>
+                        <DropdownMenuShortcut>
+                            <PlusIcon />
+                        </DropdownMenuShortcut>
+                    </DropdownMenuItem>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem>
+                        <span className="uppercase font-medium text-[#333] hover:text-black hover:font-semibold">shop by occasion</span>
+                        <DropdownMenuShortcut>
+                            <PlusIcon />
+                        </DropdownMenuShortcut>
+                    </DropdownMenuItem>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem>
+                        <span className="uppercase font-medium text-[#333] hover:text-black hover:font-semibold">valentines day</span>
+                        <DropdownMenuShortcut>
+                            <PlusIcon />
+                        </DropdownMenuShortcut>
+                    </DropdownMenuItem>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem>
+                        <span className="uppercase font-medium text-[#333] hover:text-black hover:font-semibold">shop by relation</span>
+                        <DropdownMenuShortcut>
+                            <PlusIcon />
+                        </DropdownMenuShortcut>
+                    </DropdownMenuItem>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem>
+                        <span className="uppercase font-medium text-[#333] hover:text-black hover:font-semibold">design sarees</span>
+                    </DropdownMenuItem>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem>
+                        <span className="uppercase font-medium text-[#333] hover:text-black hover:font-semibold">delivery cities</span>
+                    </DropdownMenuItem>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem>
+                        <span className="uppercase font-medium text-[#333] hover:text-black hover:font-semibold">contact</span>
+                    </DropdownMenuItem>
+                </DropdownMenuGroup>
+            </DropdownMenuContent>
+            </DropdownMenu>
+            
             MENU
-            <ul id="menu" className='absolute top-[100%] bg-red-500 transition-all duration-300'>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-                <li>4</li>
-                <li>5</li>
-            </ul>
+
         </nav>
     </header>
   )
